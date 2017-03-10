8f62d060e8b9d3948562639ba80c8c77edb6877e
JeongWonjun <nttejun@gmail.com>
2017-03-10 16:04:55 +0900

    (FibonacciEvenNumbers<4000000) = TotalPlusValue

---
+++ euler/src/eulerSolve/SolveNum2.java
@@ -0,0 +1,29 @@
+package eulerSolve;
+
+public class SolveNum2 {
+
+	public static void main(String[] args) {
+		// 피보나치 수열 짝수 4백만 이하인 모든 항을 더하면 얼마입니까?
+		
+		int maxNum = 4000000;
+		int num1 = 1;
+		int num2 = 2;
+		int num3 = 3;
+		int stepNum = 2;
+
+		for (int i = 0; i <= maxNum; i++) {
+
+			if (num3 <= maxNum) {
+				num3 = num1 + num2;
+				if (num3 % 2 == 0) {
+					stepNum = stepNum + num3;
+				}
+				num1 = num2;
+				num2 = num3;
+			}
+		}
+
+		System.out.println(stepNum);
+	}
+
+}
