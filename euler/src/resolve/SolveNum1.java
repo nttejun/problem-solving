e466f24f2d7b8100c3429eadfaec5b792c849afb
Wonjun Jeong <nttejun@gmail.com>
2017-08-01 18:44:22 +0900

    (3x || 5x)=value and 1000>value

--- euler/src/resolve/SolveNum1.java
+++ euler/src/resolve/SolveNum1.java
@@ -3,8 +3,6 @@ package resolve;
 public class SolveNum1 {
 
 	public static void main(String[] args) {
-		// 10���� ���� �ڿ��� �� 3�Ǵ� 5�� ����� 3,5,6,9 �̰�, �̰��� ��� ���ϸ� 23�Դϴ�
-		// 1000���� ���� �ڿ��� �� 3 �Ǵ� 5�� ����� ��� ���ϸ� ���ϱ�� ?
 
 		int sum = 0;
 		int result = 0;
