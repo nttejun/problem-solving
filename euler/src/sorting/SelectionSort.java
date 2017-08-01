059aaeebe10607bd3a0c468cf6c16d71fb837cdb
Wonjun Jeong <nttejun@gmail.com>
2017-08-01 18:38:21 +0900

    Selection : sort

--- euler/src/sorting/SelectionSort.java
+++ euler/src/sorting/SelectionSort.java
@@ -5,7 +5,7 @@ public class SelectionSort {
 	public static void main(String[] args) {
 
 		int[] a = {10, 900, 500, 33, 77, 200, 100, 30, 15};
-
+ 
 		for(int value=0; value<a.length; value++){
 			for(int rotation=value+1; rotation<a.length; rotation++){
 				if(a[rotation]<a[value]){
