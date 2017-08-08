6a6a093befea8c0579a04c0c6e5d2802a6cd9ef4
Wonjun Jeong <nttejun@gmail.com>
2017-08-09 05:20:15 +0900

    StringCalculator

---
+++ euler/src/resolve/StringCalculator.java
@@ -0,0 +1,38 @@
+package resolve;
+
+/**
+ * Created by nttej on 2017-08-09.
+ */
+public class StringCalculator {
+    public static void main(String[] args) {
+
+        StringCalculator stringCalculation = new StringCalculator();
+        stringCalculation.add("3,4,5;0");
+    }
+
+    public int add(String testStr) {
+
+        String[] testStrAry;
+
+        if (testStr == null || testStr.isEmpty()) {
+
+            return 1;
+
+        }
+
+        testStrAry = testStr.split(",|;");
+
+        int testInt = 0;
+
+        for (int index = 0; index < testStrAry.length; index++) {
+
+            testInt = Integer.parseInt(testStrAry[index]) + testInt;
+
+        }
+
+        return testInt;
+
+    }
+
+}
+
