c766132e85eca43af299755f15f0ea4ba8998bff
Wonjun Jeong <nttejun@gmail.com>
2017-08-12 13:20:58 +0900

    add range check method

--- euler/src/resolve/IntegerPairs.java
+++ euler/src/resolve/IntegerPairs.java
@@ -6,37 +6,20 @@ import java.util.Scanner;
  * Created by nttej on 2017-08-10.
  */
 
-// 1 IntegerPairs
-
 // (가정)
 // N개의 정수 주어졌다
 // 그 차가 K인 정수 쌍의 수
 
-// (조건)
 // 첫째 줄에 N, K 입력 >> 스페이스로 구분한다
 // 둘째 줄부터 N개의 정수 입력 >> 단 모든 정수는 그 값이 서로 달라야 한다 >> 스페이스로 구분
 
 // (출력)
 // 차가 K인 쌍의 수를 출력
 
-// (제한)
+// (조건)
 // N <= 10^5
 // 0 < K < 10^9
-// N 과 K 는 0 보다 크고 2^31 - 1 보다 작다
-
-// 예제 입력 #1
-// 5 2
-// 1 5 3 4 2
-
-// 예제 출력 #1
-// 3
-
-// 예제 입력 #2
-// 10 1
-// 363374326 364147530 61825163 1073065718 1281246024 1399469912 428047635 491595254 879792181 1069262793
-
-// 예제 출력 #2
-// 0
+// 각 정수는 0 보다 크고 2^31 - 1 보다 작다
 
 public class IntegerPairs {
 
@@ -55,7 +38,7 @@ public class IntegerPairs {
 
         setNumNK(toInts(split(scan())));
 
-        toInts(split(scan()), getNumN());
+        System.out.println("출력결과 = " + pairsCalculator(toInts(split(scan()), getNumN())));
 
     }
 
@@ -63,7 +46,7 @@ public class IntegerPairs {
 
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("plz input Number");
+        System.out.println("입력해 주세요");
 
         String inputNK = scan.nextLine();
 
@@ -71,7 +54,7 @@ public class IntegerPairs {
 
     }
 
-    public String[] split(String inputData) {
+    private String[] split(String inputData) {
 
         String[] stringData = inputData.split(" ");
 
@@ -93,21 +76,48 @@ public class IntegerPairs {
 
     }
 
-    public int toInts(String[] StringData, int numN) {
+
+    private int[] toInts(String[] StringData, int numN) {
 
         int[] toNum = new int[StringData.length];
 
         for (int index = 0; index < StringData.length; index++) {
 
-            toNum[index] = Integer.parseInt(StringData[index]);
+            toNum[index] = checkRangeN(Integer.parseInt(StringData[index]));
 
         }
 
+        checkLengthN(toNum, numN);
+
+        return toNum;
+
+    }
+
+    private int[] checkLengthN(int[] toNum, int numN){
+
         if (toNum.length != numN) {
+
+            return toInts(split(scan()), getNumN());
+
+        }   // if the input numbers is not equal to the numN go to the scan method
+
+        return toNum;
+
+    }
+
+    private int checkRangeN(int checN){
+
+        if(checN < 0 || checN > 2147483647){
 
             toInts(split(scan()), getNumN());
 
-        }   // if the input numbers is not equal to the numN go to the scan method
+        }
+
+        return checN;
+
+    }
+
+    private int pairsCalculator(int[] toNum){
 
         int result = 0;
 
@@ -115,11 +125,11 @@ public class IntegerPairs {
 
             for (int b = 0; b < a; b++) {
 
-                if (toNum[a] == toNum[b]) {
+                if (toNum[a] == toNum[b]) {   // if you have two identical equal value go to the scan method
 
                     toInts(split(scan()), getNumN());
 
-                }   // if you have two identical equal value go to the scan method
+                }
 
                 if ( ( toNum[a] - toNum[b] == getNumK() ) || ( toNum[b] - toNum[a] == getNumK()) ) {
 
@@ -135,11 +145,6 @@ public class IntegerPairs {
 
     }
 
-    private void setNumNK(int[] numNK) {
-        this.numN = numNK[0];
-        this.numK = numNK[1];
-    }
-
     private int getNumN() {
         return numN;
     }
@@ -147,5 +152,24 @@ public class IntegerPairs {
     private int getNumK(){
         return numK;
     }
+
+    private void setNumNK(int[] numNK) {
+
+        checkRange(numNK);
+
+        this.numN = numNK[0];
+        this.numK = numNK[1];
+    }
+
+    private int[] checkRange(int[] numNK){
+
+        if(numNK[0] > 100000){ return toInts(split(scan())); }
+
+        if(numNK[1] > 1000000000){ return toInts(split(scan())); }
+
+        return numNK;
+
+    }
+
 
 }
