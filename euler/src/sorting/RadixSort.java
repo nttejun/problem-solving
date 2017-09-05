e5574b9b482a417f07d54585189c122bbe7b056c
Wonjun Jeong <nttejun@gmail.com>
2017-09-05 21:45:11 +0900

    inputList in Bucket index

--- euler/src/sorting/RadixSort.java
+++ euler/src/sorting/RadixSort.java
@@ -7,18 +7,15 @@ import java.util.List;
  * Created by nttej on 2017-09-04.
  */
 
-// 시간복잡도 O(dn)
-
-
 public class RadixSort {
 
     public static final int max = 10;
-    public static final int infinity = 99999;
 
     public void sorting(int[] inputList){
 
         int digit = maxDigit(maxDigitValue(inputList));
         List<List<Integer>> bucket = makeBucket(inputList);
+        checkDigit(inputList, digit, bucket);
 
     }
 
@@ -59,7 +56,7 @@ public class RadixSort {
 
         List<List<Integer>> bucket = new ArrayList<>(max);
 
-        for(int a = 0; a < inputList.length; a++){
+        for(int a = 0; a < max; a++){
 
             bucket.add(new ArrayList<>());
 
@@ -69,11 +66,45 @@ public class RadixSort {
 
     }
 
-    public void inputDataInBuncket(int[] inputList, int length, List<List> bucket){
-        
+    public void checkDigit(int[] inputList, int length, List<List<Integer>> bucket) {
+
+        int size = length;
+
+        int divideNumber = 1;
+
+        while (size != 0) {
+
+            size --;
+
+            inputDataInBucket(inputList, bucket, divideNumber);
+
+            divideNumber = divideNumber * 10;
+
+        }
     }
 
-    public void dataOutInBucket(){
+    public List<List<Integer>> inputDataInBucket(int[] inputList, List<List<Integer>> bucket, int divideNumber) {
+
+        for (int index = 0; index < inputList.length; index++) {
+
+            bucket.get((inputList[index] / divideNumber) % 10).add(inputList[index]);
+
+        }
+
+        for(int a = 0; a < bucket.size(); a++){
+            System.out.println("버킷 인덱스 = " + a);
+            for(int b = 0; b < bucket.get(a).size(); b++ ) {
+                System.out.println(bucket.get(a).get(b));
+            }
+        }
+
+        return bucket;
+    }
+
+
+
+    public void dataOutInBucket(List<List<Integer>> bucket){
+
 
     }
 
@@ -82,6 +113,7 @@ public class RadixSort {
         int[] inputList = {999, 22, 555, 33, 46, 88, 11, 0, 9};
 
         RadixSort radixSort = new RadixSort();
+
         radixSort.sorting(inputList);
 
     }
