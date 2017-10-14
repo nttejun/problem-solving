aedd9f4c338f85961696ac27c3be61548c05382a
Wonjun Jeong <nttejun@gmail.com>
2017-10-14 13:39:18 +0900

    add 1, 2, 3 equal n case

---
+++ euler/src/resolve/Backjoon9095.java
@@ -0,0 +1,41 @@
+package resolve;
+
+import java.io.*;
+
+/**
+ * Created by nttej on 2017-10-13.
+ */
+// Backjoon 9095
+// 1 +,  2 +,  3 + = n case
+public class Backjoon9095 {
+
+    private static int addCheck(int num) {
+
+        if (num == 0) {
+            return 1;
+        } else if (num < 0) {
+            return 0;
+        } else {
+            int sum = 0;
+            for (int i = 3; i > 0; i--) {
+                sum += addCheck(num - i);
+            }
+            return sum;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Backjoon9095 addDigit = new Backjoon9095();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+
+            int inputNumber = Integer.parseInt(br.readLine());
+            System.out.println(addDigit.addCheck(inputNumber));
+
+        }
+    }
+}
+
