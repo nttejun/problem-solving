a54510f28ff2f3b0c5e66f732910928af51f71be
Wonjun Jeong <nttejun@gmail.com>
2017-10-25 09:21:59 +0900

    add Bottom up Dynamic programming

--- euler/src/resolve/Backjoon1463.java
+++ euler/src/resolve/Backjoon1463.java
@@ -1,5 +1,6 @@
 package resolve;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -10,13 +11,20 @@ public class Backjoon1463 {
 
     static int[] d;
 
-    public static int start(int n){
+    public static int start(int n) {
 
         if (n == 1) {
             return 0;
         }
 
         d[n] = start(n - 1) + 1;
+
+        if (n % 3 == 0) {
+            int temp = d[n / 3];
+            if (temp < d[n]) {
+                d[n] = temp + 1;
+            }
+        }
 
         if (n % 2 == 0) {
             int temp = d[n / 2];
@@ -25,13 +33,34 @@ public class Backjoon1463 {
             }
         }
 
-        if (n % 3 == 0) {
-            int temp = d[n / 3];
-            if (temp < d[n]) {
-                d[n] = temp + 1;
-            }
-        }
         return d[n];
+    }
+
+    // -1 로 d[i]를 먼저 만들고
+    // 2로 나눠지고 d[i]가 2로 나눈 값보다 크면 d[i]는 2으로 나눈 값으로 교체
+    // 3로 나눠지고 d[i]가 3로 나눈 값보다 크면 d[i]는 3으로 나눈 값으로 교체
+    public static int bottomUp(int n) {
+
+        int result = 0;
+
+        d[1] = 0;
+
+        for (int i = 2; i <= n; i++) {
+
+            d[i] = d[i - 1] + 1;
+
+            if (i % 2 == 0 && d[i] > d[i / 2] + 1) {
+                d[i] = d[i / 2] + 1;
+            }
+
+            if (i % 3 == 0 && d[i] > d[i / 3] + 1) {
+                d[i] = d[i / 3] + 1;
+            }
+
+            result = d[i];
+        }
+
+        return result;
     }
 
     public static void main(String[] args) {
@@ -44,9 +73,11 @@ public class Backjoon1463 {
             n = scanner.nextInt();
         }
 
-        d = new int[n+1];
+        d = new int[n + 1];
 
         Backjoon1463 backjoon1436 = new Backjoon1463();
-        System.out.println(backjoon1436.start(n));
+
+        System.out.println(backjoon1436.bottomUp(n));
+
     }
 }
