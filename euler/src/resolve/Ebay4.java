a07c7e2e397e1bb95b6a4d18db834e885b370be7
Wonjun Jeong <nttejun@gmail.com>
2017-11-12 21:49:14 +0900

    Ebay 4 number 20171112

---
+++ euler/src/resolve/Ebay4.java
@@ -0,0 +1,62 @@
+package resolve;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Created by nttej on 2017-11-12.
+ */
+
+/*
+
+[입력 값]
+입력 값 범위
+1 <= A, K <= 100
+0 <= P <= 100
+
+[샘플 1]
+3 5 1 = ?
+
+[샘플 2]
+4 5 1 = 2
+
+*/
+
+public class Ebay4 {
+
+    static int minNum(int a, int k, int p) {
+
+        int divide = p;
+
+        int minus = k-a;
+
+        // 따라잡지 못하는 경우
+        if(a>k){
+
+            return -1;
+
+        }
+
+        int day = p / minus;
+
+        if(p % minus == 0) {
+
+            day++;
+
+        }
+
+        return day;
+
+    }
+
+    @Test
+    public void possible() {
+        assertEquals(2, minNum(4, 5, 1));
+    }
+
+    @Test
+    public void impossible(){
+        assertEquals(-1, minNum(5, 3, 1) );
+    }
+}
