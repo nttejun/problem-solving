f064aa1b85a39e706a78cd5c7b64f12c526ec9fb
won jun <nttejun@gmail.com>
2018-12-21 17:37:35 +0900

    Number less than x

---
+++ euler/src/resolve/Baekjoon10871.java
@@ -0,0 +1,31 @@
+package resolve;
+
+import java.util.Scanner;
+
+public class Baekjoon10871 {
+
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        int count = scan.nextInt();
+
+        int x = scan.nextInt();
+
+        int[] seqVal = new int[count];
+
+        for(int n = 0; n < count; n++){
+            seqVal[n] = scan.nextInt();
+        }
+
+        for(int val : seqVal){
+
+            if(x > val){
+
+                System.out.print(val + " ");
+
+            }
+        }
+
+    }
+}
