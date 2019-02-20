3f3a855b249784c728dfecdf553e54352681e01d
won jun <nttejun@gmail.com>
2019-02-21 00:37:44 +0900

    LIS dynamic programming

--- euler/src/resolve/Baekjoon11053.java
+++ euler/src/resolve/Baekjoon11053.java
@@ -10,9 +10,9 @@ public class Baekjoon11053 {
         int max = 1;
 
         for(int i=0; i<n; i++){
-            dp[1][0] = 1;
+            dp[i][0] = 1;
             dp[i][1] = sc.nextInt();
-            for(int ii = 0; ii < i; i++){
+            for(int ii = 0; ii < i; ii++){
                 if(dp[ii][1] < dp[i][1])
                     dp[i][0] = Math.max(dp[i][0], dp[ii][0] + 1);
             }
