006dbfe5489cebf7ea9f10c56c53e164bf090bb3
won jun <nttejun@gmail.com>
2019-05-15 23:01:32 +0900

    Mark star 3

---
+++ euler/src/resolve/Baekjoon2440.java
@@ -0,0 +1,23 @@
+package resolve;
+
+import java.util.Scanner;
+
+public class Baekjoon2440 {
+    public static void main(String[] args) {
+        Baekjoon2440 baekjoon2440 = new Baekjoon2440();
+        baekjoon2440.markStar();
+    }
+
+    private void markStar() {
+        StringBuilder stringBuilder = new StringBuilder();
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        for(int i=1; i<=N; i++){
+            for (int j=i; j<=N; j++){
+                stringBuilder.append("*");
+            }
+            stringBuilder.append("\n");
+        }
+        System.out.println(stringBuilder.toString());
+    }
+}
