57bca41ed821c612f09d1884601b7b64389cfc90
won jun <nttejun@gmail.com>
2019-05-19 13:05:45 +0900

    Mark star 6

--- euler/src/resolve/Baekjoon2443.java
+++ euler/src/resolve/Baekjoon2443.java
@@ -3,7 +3,7 @@ package resolve;
 import java.util.Scanner;
 
 public class Baekjoon2443 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Baekjoon2443 baekjoon2443 = new Baekjoon2443();
         baekjoon2443.markStar();
     }
@@ -12,15 +12,17 @@ public class Baekjoon2443 {
         StringBuilder stringBuilder = new StringBuilder();
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
-        for(int i=1; i<=N; i++){
-            for(int j=N-i; j<N; j++) {
+        int s = N * 2 - 1 ;
+        for (int i = 1; i <= N; i++) {
+            for (int j = N - i + 1; j < N; j++) {
                 stringBuilder.append(" ");
             }
-            for(int j=i; j<=N; j++) {
+            for (int j = 1; j <= s; j++) {
                 stringBuilder.append("*");
             }
+            s = s-2 ;
+            stringBuilder.append("\n");
         }
-
         System.out.println(stringBuilder.toString());
     }
 }
