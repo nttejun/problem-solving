2a44debdbf7b44e5c8699ae182737fa5d643448b
nttejun <nttejun@gmail.com>
2019-07-03 06:12:04 +0900

    Programmers level1 a player who has not finished

--- euler/src/resolve/ProgLevel1Completion.java
+++ euler/src/resolve/ProgLevel1Completion.java
@@ -1,10 +1,24 @@
 package resolve;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class ProgLevel1Completion {
+
+    public String solution1(String[] participant, String[] completion) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        int i;
+        for( i=0; i<completion.length; i++){
+            if(!participant[i].equals(completion[i])){
+                return participant[i];
+            }
+        }
+        return participant[i];
+    }
 
 
     public String solution(String[] participant, String[] completion) {
@@ -13,7 +27,6 @@ public class ProgLevel1Completion {
         for (int i = 0; i < participant.length; i++) {
             for (int j = 0; j < completion.length; j++) {
                 if (participant[i] == completion[j]) {
-                    System.out.println(">>>>" + participant[i]);
                     list.remove(participant[i]);
                 }
             }
@@ -26,6 +39,8 @@ public class ProgLevel1Completion {
         String[] participant = {"t", "tt", "ttt"};
         String[] completion = {"t", "tt"};
         ProgLevel1Completion p = new ProgLevel1Completion();
-        p.solution(participant, completion);
+        //p.solution(participant, completion);
+        //p.solution1(participant, completion);
+        p.solution2(participant, completion);
     }
 }
