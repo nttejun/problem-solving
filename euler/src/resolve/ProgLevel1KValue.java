1860f8cac57fca7fbb86f47157639d8bd94a0e40
nttejun <nttejun@gmail.com>
2019-07-06 01:57:39 +0900

    Added solution  array copy method

--- euler/src/resolve/ProgLevel1KValue.java
+++ euler/src/resolve/ProgLevel1KValue.java
@@ -31,13 +31,28 @@ public class ProgLevel1KValue {
         return answer;
     }
 
+    public int[] solution1(int[] array, int[][] commands){
+
+        int[] answer = new int[commands.length];
+
+        for(int i=0; i<commands.length; i++){
+            int[] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2]-1];
+            System.out.println(answer[i]);
+        }
+
+        return answer;
+
+    }
+
     public static void main(String[] args){
 
         int ary[] = {1, 5, 2, 6, 3, 7, 4};
         int com[][] = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
 
         ProgLevel1KValue p = new ProgLevel1KValue();
-        p.solution(ary, com);
+        p.solution1(ary, com);
     }
 
 }
