abb6b20ede7346b2af5b4f7c3be0938ade411137
nttejun <nttejun@gmail.com>
2019-07-11 21:58:21 +0900

    Added number not eqaul

--- euler/src/resolve/ProgLv1NumberNotEqual.java
+++ euler/src/resolve/ProgLv1NumberNotEqual.java
@@ -7,17 +7,12 @@ import java.util.List;
 public class ProgLv1NumberNotEqual {
     public int[] solution(int[] arr) {
         List<Integer> answer = new ArrayList<>();
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+        answer.add(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
             answer.add(arr[i]);
             int size = answer.size()-1;
-            System.out.println("size : " + answer.size());
-            for (int j = 0; j < size; j++) {
-                if (arr[i] == answer.get(j)) {
-                    System.out.println( " >>>> " + answer.get(size));
-                    answer.remove(size);
-                    break;
-                }
+            if (answer.get(size-1) == answer.get(size)) {
+                answer.remove(size);
             }
         }
 
@@ -29,11 +24,11 @@ public class ProgLv1NumberNotEqual {
         }
         return result;
     }
-
     public static void main(String[] args){
 
         ProgLv1NumberNotEqual p = new ProgLv1NumberNotEqual();
         int[] a = {1, 20, 20, 4, 4, 30};
+
         System.out.println(p.solution(a));
     }
 }
