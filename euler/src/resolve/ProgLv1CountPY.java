adb31d7c030ae7b4e2e9431a004c2dc52f217790
nttejun <nttejun@gmail.com>
2019-07-15 21:12:04 +0900

    Added using lambda

--- euler/src/resolve/ProgLv1CountPY.java
+++ euler/src/resolve/ProgLv1CountPY.java
@@ -37,8 +37,14 @@ public class ProgLv1CountPY {
         return answer;
     }
 
+    boolean solution1(String s){
+        s = s.toUpperCase();
+        return s.chars().filter( e -> 'P' == e).count() == s.chars().filter(e -> 'Y' == e).count();
+    }
+
     public static void main(String[] args){
         ProgLv1CountPY progLv1CountPY = new ProgLv1CountPY();
         progLv1CountPY.solution("pPooooyY");
+        progLv1CountPY.solution1("pPooooyY");
     }
 }
