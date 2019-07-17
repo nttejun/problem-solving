f649abd299e3f9be3429d41625862a70df696c0a
nttejun <nttejun@gmail.com>
2019-07-17 22:20:32 +0900

    Added check num using regex matches

--- euler/src/resolve/ProgLv1ChkNumber.java
+++ euler/src/resolve/ProgLv1ChkNumber.java
@@ -1,16 +1,13 @@
 package resolve;
 
-
-import java.util.regex.Pattern;
-
 public class ProgLv1ChkNumber {
     public boolean solution(String s) {
-        return s.matches(String.valueOf("^[0-9]*$"));
-
+        if(s.length() == 4 || s.length() == 6) return s.matches("^[0-9]*$");
+        return false;
     }
 
     public static void main(String[] args){
         ProgLv1ChkNumber progLv1ChkNumber = new ProgLv1ChkNumber();
-        System.out.println(progLv1ChkNumber.solution("123"));
+        System.out.println(progLv1ChkNumber.solution("123das"));
     }
 }
