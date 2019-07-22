067c82d8f8f05a1c52ad98479b176df1bbb27690
nttejun <nttejun@gmail.com>
2019-07-22 11:49:59 +0900

    Added solution1 more efficient b

--- euler/src/resolve/ProgLv1SumOfDivisors.java
+++ euler/src/resolve/ProgLv1SumOfDivisors.java
@@ -25,16 +25,26 @@ public class ProgLv1SumOfDivisors {
                 }
             }
         }
-        Iterator listIterator = list.iterator();
         for(int temp : list)
             answer += temp;
 
         return answer;
     }
+
+    public int solution1(int n){
+
+        int answer =0;
+        for(int i=1; i<=n/2; i++){
+            if(n%i==0) answer+=i;
+        }
+        return answer+n;
+    }
+
     public static void main(String[] args){
 
         ProgLv1SumOfDivisors progLv1SumOfDivisors = new ProgLv1SumOfDivisors();
         System.out.println(progLv1SumOfDivisors.solution(12));
+        System.out.println(progLv1SumOfDivisors.solution1(56));
 
     }
 }
