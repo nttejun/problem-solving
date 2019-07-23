9ea20d7fe18b2584d569eeb411e43d153bd5aae7
nttejun <nttejun@gmail.com>
2019-07-23 21:54:15 +0900

    Updated stack code

--- euler/src/resolve/ProgLv2PriceDownTime.java
+++ euler/src/resolve/ProgLv2PriceDownTime.java
@@ -1,6 +1,7 @@
 package resolve;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 public class ProgLv2PriceDownTime {
     public int[] solution(int[] prices) {
@@ -22,10 +23,33 @@ public class ProgLv2PriceDownTime {
         return answer;
     }
 
+    public int[] solution1(int[] prices){
+        Stack<Integer> indexs = new Stack<>();
+        int i=0;
+        int[] terms = new int[prices.length];
+
+        indexs.push(i);
+        for(i=1; i<prices.length; i++){
+            while (!indexs.empty() && prices[i] < prices[indexs.peek()]){
+                int index = indexs.pop();
+                terms[index] = i - index;
+            }
+            indexs.push(i);
+        }
+
+        while (!indexs.empty()){
+            int index = indexs.pop();
+            terms[index] = i - index - 1;
+        }
+
+        return terms;
+    }
 
     public static void main(String[] args) {
         ProgLv2PriceDownTime progLv2PriceDownTIme = new ProgLv2PriceDownTime();
         System.out.println(Arrays.toString(progLv2PriceDownTIme.solution(new int[]{1, 2, 3, 2, 3})));
-        System.out.println(Arrays.toString(progLv2PriceDownTIme.solution(new int[]{5, 4, 3, 5, 6, 5})));
+        System.out.println(Arrays.toString(progLv2PriceDownTIme.solution1(new int[]{1, 2, 3, 2, 3})));
+        System.out.println(Arrays.toString(progLv2PriceDownTIme.solution1(new int[]{5, 4, 3, 5, 6, 5})));
+        System.out.println(Arrays.toString(progLv2PriceDownTIme.solution1(new int[]{5, 4, 3, 5, 6, 5})));
     }
 }
