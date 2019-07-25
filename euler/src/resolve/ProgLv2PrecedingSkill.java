bb279261b949f0e3eaf5ffb0f092147d88a1f99a
nttejun <nttejun@gmail.com>
2019-07-26 01:22:00 +0900

    Added init setting

---
+++ euler/src/resolve/ProgLv2PrecedingSkill.java
@@ -0,0 +1,18 @@
+package resolve;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProgLv2PrecedingSkill {
+    public int solution(String skill, String[] skill_tree){
+        int answer = 0;
+
+        List list = new ArrayList();
+
+        for(int i=0; i<skill.length(); i++){
+            list.add(skill.charAt(i));
+        }
+
+        return answer;
+    }
+}
