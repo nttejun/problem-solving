4cde5e0f9c64d1ba1c7b43359e76aa139e10f1d7
nttejun <nttejun@gmail.com>
2019-08-05 05:55:10 +0900

    Added develop function

--- euler/src/resolve/ProgLv2DevelopTerm.java
+++ euler/src/resolve/ProgLv2DevelopTerm.java
@@ -1,34 +1,30 @@
 package resolve;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
+import java.sql.Array;
+import java.util.*;
 import java.util.concurrent.ConcurrentLinkedDeque;
 
 public class ProgLv2DevelopTerm {
     public int[] solution(int[] progresses, int[] speeds) {
 
         int[] remainProg = new int[progresses.length];
-        int[] days = new int[progresses.length];
-
         for (int i = 0; i < progresses.length; i++) {
-
             remainProg[i] = 100 - progresses[i];
-
         }
 
-        int rProg = 0;
+        int prog = 0;
+        int[] days = new int[progresses.length];
 
         for(int i=0; i<progresses.length; i++){
 
-            rProg = speeds[i];
+            prog = speeds[i];
 
-            for(int j=0; j<=101; j++){
-                if(remainProg[i] <= rProg) {
+            for(int j=0; j<=100000000; j++){
+                if(remainProg[i] <= prog) {
                     days[i] = j+1;
                     break;
                 }
-                rProg = rProg + speeds[i];
+                prog = prog + speeds[i];
             }
         }
 
@@ -68,8 +64,65 @@ public class ProgLv2DevelopTerm {
             );
         }
 
-        int[] answer = {};
+        List<Integer> result = new ArrayList<>();
+        int standard = q.poll();
+        int cnt = 1;
+
+        while (!q.isEmpty()){
+            int num = q.poll();
+            if(standard >= num){
+                cnt++;
+            } else {
+                result.add(cnt);
+                cnt = 1;
+                standard = num;
+            }
+        }
+
+        result.add(cnt);
+        int[] answer = new int[result.size()];
+
+        for(int i=0; i<answer.length; i++){
+            answer[i] = result.get(i);
+        }
         return answer;
+    }
+
+    public int[] solution2(int[] progresses, int[] speeds) {
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> answerList = new ArrayList<>();
+
+        for(int i = 0; i < speeds.length; i++){
+            double remain = (100-progresses[i] / (double) speeds[i]);
+            int date = (int) Math.ceil(remain);
+
+            if(!q.isEmpty() && q.peek() < date){
+                answerList.add(q.size());
+                q.clear();
+            }
+
+            q.offer(date);
+        }
+
+        answerList.add(q.size());
+
+        int[] answer = new int[answerList.size()];
+        for(int i=0; i<answer.length; i++){
+            answer[i] = answerList.get(i);
+        }
+        return answer;
+    }
+
+    public int[] solution3(int[] progresses, int[] speeds) {
+        int[] dayOfend = new int[100];
+        int day = -1;
+        for(int i=0; i<progresses.length; i++){
+            while (progresses[i] + (day*speeds[i]) < 100){
+                day++;
+            }
+            dayOfend[day]++;
+        }
+        return Arrays.stream(dayOfend).filter(i -> i!=0).toArray();
     }
 
         public static void main(String[] args){
