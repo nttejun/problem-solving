cd0ddce58cc6cef5b59aca0b4a93fd127e0f1bdc
nttejun <nttejun@gmail.com>
2019-08-08 00:17:23 +0900

    Updated balance check

--- euler/src/resolve/ProgLv2Laser.java
+++ euler/src/resolve/ProgLv2Laser.java
@@ -5,13 +5,15 @@ import java.util.concurrent.ConcurrentLinkedDeque;
 
 public class ProgLv2Laser {
     public int solution(String arrangement) {
-        int answer = 0;
 
         Queue<String> q = new ConcurrentLinkedDeque<>();
 
         for (int i = 0; i < arrangement.length(); i++) {
             q.add(String.valueOf(arrangement.charAt(i)));
         }
+
+        int answer = 0;
+        int balance = 0;
 
         String qNow = q.poll();
         String qNext = q.poll();
@@ -20,14 +22,23 @@ public class ProgLv2Laser {
 
 
             if (qNow == "(") {
+                balance++;
 
                 if (qNext == "(") {
+                    balance++;
+                    answer++;
 
                 } else if (qNext == ")"){
+                    balance--;
+
+                    if(balance > 0){
+                        answer++;
+                    }
 
                 }
 
             } else if (qNow == ")") {
+                 balance--;
 
                 if (qNext == "(") {
 
