ac1a88161dca8fa2dadc3731ca7b88906098d82b
nttejun <nttejun@gmail.com>
2019-08-20 23:52:13 +0900

    Added find day of the week

---
+++ euler/src/resolve/BaekjoonBookFindDay21990702.java
@@ -0,0 +1,18 @@
+package resolve;
+
+/***
+ * 2199년 7월 2일
+ * 무슨 요일인지 구하시오
+ */
+public class BaekjoonBookFindDay21990702 {
+    public static void main(String[] args) {
+
+    /***
+     * if ((year % 4 == 0) && (year % 100 != 0))
+     * then 윤년
+     * if ((year % 4 == 0) && (year % 100 == 0) && (year % 400 == 0))
+     * then 윤년
+     */
+
+    }
+}
