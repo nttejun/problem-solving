d9cb45ec3a4737039c0ba709695e55d05cbc76ae
nttejun <nttejun@gmail.com>
2019-10-13 10:37:59 +0900

    Added bubble sort

---
+++ euler/src/sorting/BubbleSortRe.java
@@ -0,0 +1,36 @@
+package sorting;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BubbleSortRe {
+
+    public int[] bubbleSorting(int[] unsortValue) {
+
+        int temp;
+        for(int i=0; i<unsortValue.length; i++){
+            temp = unsortValue[i];
+            for(int j=i+1; j<unsortValue.length; j++){
+                if(temp > unsortValue[j]){
+                    unsortValue[i] = unsortValue[j];
+                    unsortValue[j] = temp;
+                    temp = unsortValue[i];
+                }
+            }
+        }
+        return unsortValue;
+    }
+
+
+    @Test
+    public void test_bubbleSorting() {
+        int[] unsortValue = {1, 10, 5, 8, 7, 6, 4, 3, 2, 9};
+        int[] sortedValue = bubbleSorting(unsortValue);
+        for(int i = 0; i < sortedValue.length; i++){
+            assertEquals(i+1, sortedValue[i]);
+        }
+    }
+}
+
+
