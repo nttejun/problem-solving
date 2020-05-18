ce044a771fbcea6b56d0b6cfd7dace9d8dd4b880
nttejun <nttejun@gmail.com>
2020-05-19 01:19:54 +0900

    Doble loop

---
+++ euler/src/programmers/HashLv2isPrefixPhonebook.java
@@ -0,0 +1,33 @@
+package programmers;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HashLv2isPrefixPhonebook {
+  public boolean solution(String[] phone_book){
+    for (int i = 0; i< phone_book.length; i++) {
+      for (int j = 0; j< phone_book.length; j++) {
+        if (i == j) continue;
+        if (phone_book[i].startsWith(phone_book[j])) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  @Test
+  public void TEST_01() {
+    Assert.assertFalse(solution(new String[]{"119", "97674223", "1195524421"}));
+  }
+
+  @Test
+  public void TEST_02() {
+    Assert.assertTrue(solution(new String[]{"123", "456", "789"}));
+  }
+
+  @Test
+  public void TEST_03() {
+    Assert.assertFalse(solution(new String[]{"12", "123", "1235", "567", "88"}));
+  }
+}
