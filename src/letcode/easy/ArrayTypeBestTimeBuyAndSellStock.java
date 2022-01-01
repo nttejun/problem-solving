e168a88a29ca3bd80614ab943e2d65bf92187902
nttejun <nttejun@gmail.com>
2022-01-01 12:10:44 +0900

    [Add] best time buy and sell stock 테스트 케이스 1번 통과 코드

---
+++ src/letcode/easy/ArrayTypeBestTimeBuyAndSellStock.java
@@ -0,0 +1,30 @@
+package letcode.easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ArrayTypeBestTimeBuyAndSellStock {
+  public int maxProfit(int[] prices) {
+    int profit = 0;
+    for (int i = 0; i < prices.length-1; i++) {
+      if (prices[i] < prices[i+1]) {
+        if (prices[i+1] < prices [i+2])
+        profit += prices[i+1] - prices[i];
+        i++;
+      }
+    }
+    return profit;
+  }
+
+  @Test
+  public void getMaxProfit_TEST_CASE1() {
+    int[] prices = {7,1,5,3,6,4};
+    Assert.assertEquals(7, maxProfit(prices));
+  }
+
+  @Test
+  public void getMaxProfit_TEST_CASE2() {
+    int[] prices = {1,2,3,4,5};
+    Assert.assertEquals(4, maxProfit(prices));
+  }
+}
