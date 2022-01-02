5dc461ca136c205b94d5f0e71978765e3753d766
nttejun <nttejun@gmail.com>
2022-01-02 17:33:45 +0900

    [Add] intersection of two arrays 파일

---
+++ src/letcode/easy/ArrayTypeIntersectionTwoArrays.java
@@ -0,0 +1,40 @@
+package letcode.easy;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ArrayTypeIntersectionTwoArrays {
+  public int[] intersect(int[] nums1, int[] nums2) {
+    Map<Integer, Integer> map = new HashMap<>();
+    List<Integer> intersectNums = new ArrayList<>();
+    for (int i = 0; i < nums1.length; i++) {
+      map.put(nums1[i], 1);
+    }
+
+    for (int i = 0; i < nums2.length; i++) {
+      if (map.get(nums2[i]) != null) {
+        intersectNums.add(nums2[i]);
+      }
+    }
+    return intersectNums.stream().mapToInt(n -> n).toArray();
+  }
+
+  @Test
+  public void intersectionTwoArray_테스트_CASE1() {
+    int[] numsFirst = {1, 2, 2, 1};
+    int[] numsSeconds = {2, 2};
+    System.out.println(intersect(numsFirst, numsSeconds));
+    Assert.assertEquals(new int[]{2, 2}, intersect(numsFirst, numsSeconds));
+  }
+
+  @Test
+  public void intersectionTwoArray_테스트_CASE2() {
+    int[] numsFirst = {4, 9, 5};
+    int[] numsSeconds = {9, 4, 9, 8, 4};
+    Assert.assertEquals(new int[]{4, 9}, intersect(numsFirst, numsSeconds));
+  }
+}
