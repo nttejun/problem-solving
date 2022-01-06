7f5cc400c1661cd153d4a32c0ee06c9a946a907b
nttejun <nttejun@gmail.com>
2022-01-06 21:22:02 +0900

    [Modify] reverse integer 테스트 케이스 추가

--- src/letcode/easy/StringTypeReverseInteger.java
+++ src/letcode/easy/StringTypeReverseInteger.java
@@ -5,17 +5,43 @@ import org.junit.Test;
 
 public class StringTypeReverseInteger {
   public int reverse(int x) {
-    int result = 0;
-    while (x > 0) {
-      result = x % 10 + result * 10 ;
-      x = x / 10;
+    boolean isMinus = (x < 0) ? true : false;
+    long xNum = Math.abs(x);
+    long result = 0;
+    while (xNum > 0) {
+      result = xNum % 10 + result * 10 ;
+      xNum = xNum / 10;
     }
-    return result;
+
+    result = (isMinus) ? -result : result;
+    if (2147483647 < result || -2147483647 > result) {
+      return 0;
+    }
+
+    return (int) result;
   }
 
   @Test
   public void reverse_테스트_CASE1() {
     int num = 123;
     Assert.assertEquals(321, reverse(num));
+  }
+
+  @Test
+  public void reverse_테스트_CASE2() {
+    int num = -123;
+    Assert.assertEquals(-321, reverse(num));
+  }
+
+  @Test
+  public void reverse_테스트_CASE3() {
+    int num = 120;
+    Assert.assertEquals(21, reverse(num));
+  }
+
+  @Test
+  public void reverse_테스트_CASE4() {
+    int num = 1534236469;
+    Assert.assertEquals(0, reverse(num));
   }
 }
