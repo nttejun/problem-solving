ed49e5ed0639b811155d258c046241aec0589acf
nttejun <nttejun@gmail.com>
2022-02-13 23:32:08 +0900

    [Add] remove nth node from end of list

--- src/letcode/easy/LinkedListTypeRemoveNthNodeFromEndofList.java
+++ src/letcode/easy/LinkedListTypeRemoveNthNodeFromEndofList.java
@@ -21,6 +21,14 @@ public class LinkedListTypeRemoveNthNodeFromEndofList {
   }
 
   public ListNode removeNthFromEnd(ListNode head, int n) {
+    ListNode fast = head, slow = head;
+    for (int i = 0; i < n; i++) fast = fast.next;
+    if (fast == null) return head.next;
+    while (fast.next != null) {
+      fast = fast.next;
+      slow = slow.next;
+    }
+    slow.next = slow.next.next;
     return head;
   }
 }
