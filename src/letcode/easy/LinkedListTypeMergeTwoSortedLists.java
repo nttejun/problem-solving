02093d8cab4f1fd8a99e1aeb0745bde0971c2e03
nttejun <nttejun@gmail.com>
2022-02-15 20:04:44 +0900

    [Add] merge two sorted lists

---
+++ src/letcode/easy/LinkedListTypeMergeTwoSortedLists.java
@@ -0,0 +1,15 @@
+package letcode.easy;
+
+public class LinkedListTypeMergeTwoSortedLists {
+  public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+  public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+    return null;
+  }
+}
