fd47404fcdf4797b6a61ea68594a17534536ab2e
nttejun <nttejun@gmail.com>
2022-02-16 22:49:41 +0900

    [Add] merge two sorted lists

--- src/letcode/easy/LinkedListTypeMergeTwoSortedLists.java
+++ src/letcode/easy/LinkedListTypeMergeTwoSortedLists.java
@@ -10,6 +10,13 @@ public class LinkedListTypeMergeTwoSortedLists {
   }
 
   public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+    if(list1 == null) {
+      return list2;
+    }
+
+    if(list2 == null) {
+      return list1;
+    }
     return null;
   }
 }
