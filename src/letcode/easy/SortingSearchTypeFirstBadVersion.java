404281cee81e32be9fae2ffe80e94ef04a73a562
nttejun <nttejun@gmail.com>
2022-02-27 10:29:59 +0900

    [Add] first bad version

--- src/letcode/easy/SortingSearchTypeFirstBadVersion.java
+++ src/letcode/easy/SortingSearchTypeFirstBadVersion.java
@@ -1,10 +1,36 @@
 package letcode.easy;
 
-public class SortingSearchTypeFirstBadVersion {
-  public int firstBadVersion(int n) {
-    int lo = 1;
-    int hi = n;
+import org.junit.Assert;
+import org.junit.Test;
 
-    return lo;
+public class SortingSearchTypeFirstBadVersion {
+
+  public int firstBadVersion(int n) {
+    int left = 1;
+    int right = n;
+
+    while (left < right) {
+      int mid = left + (right - left) / 2;
+      if (isBadVersion(mid)) {
+        right = mid;
+      } else {
+        left = 1 + mid;
+      }
+    }
+    return left;
+  }
+
+  private boolean isBadVersion(int mid) {
+    int firstBadVersion = 4;
+    if (mid > firstBadVersion) {
+      return true;
+    }
+    return false;
+  }
+
+  @Test
+  public void TEST_CASE_1() {
+    SortingSearchTypeFirstBadVersion firstBadVersion = new SortingSearchTypeFirstBadVersion();
+    Assert.assertEquals(firstBadVersion.firstBadVersion(5), 5);
   }
 }
