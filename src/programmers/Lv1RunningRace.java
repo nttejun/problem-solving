cf74f737e714afe6fa0953d6843d0ca407bea93a
nttejun <nttejun@gmail.com>
2023-04-14 19:30:36 +0900

    [Add] lv1 running race java8 stream 활용한 코드로 변경

--- src/programmers/Lv1RunningRace.java
+++ src/programmers/Lv1RunningRace.java
@@ -1,7 +1,9 @@
 package programmers;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.IntStream;
 import org.junit.Test;
 
 public class Lv1RunningRace {
@@ -33,5 +35,36 @@ public class Lv1RunningRace {
 
     String[] answer = list.values().toArray(new String[0]);
     System.out.println(String.join(", ", answer));
+  }
+
+  private static Map<String, Integer> playerInfo = new HashMap<>();
+
+  public String[] solution(String[] players, String[] callings) {
+    makeInfo(players);
+
+    Arrays.stream(callings).forEach(player -> {
+      int index = playerInfo.get(player);
+      String frontPlayer = players[index -1];
+      players[index] = frontPlayer;
+      players[index-1] = player;
+
+      playerInfo.put(frontPlayer, index);
+      playerInfo.put(player, index-1);
+    });
+
+    return players;
+  }
+
+  private void makeInfo(String[] players) {
+    IntStream.range(0, players.length).forEach(index -> {
+      playerInfo.put(players[index], index);
+    });
+  }
+
+  public static void main(String[] args) {
+    Lv1RunningRace lv1RunningRace = new Lv1RunningRace();
+    String[] players = {"mumu", "soe", "poe", "kai", "mine"};
+    String[] callings = {"kai", "kai", "mine", "mine"};
+    lv1RunningRace.solution(players, callings);
   }
 }
