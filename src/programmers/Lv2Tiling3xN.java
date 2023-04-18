5653b25e33a7a48d75c4ab72adcf6e4937914842
nttejun <nttejun@gmail.com>
2023-04-18 09:21:36 +0900

    [Add] lv2 3 x n tiling (dp)

---
+++ src/programmers/Lv2Tiling3xN.java
@@ -0,0 +1,23 @@
+package programmers;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12902
+public class Lv2Tiling3xN {
+  public int solution(int n) {
+    int answer = 0;
+    int mod = 1000000007;
+    int[] dp = new int[n+1];
+    dp[1] = 2;
+    dp[2] = 3;
+    for(int i = 3; i <= n; i++){
+      if(i%2 == 0){
+        //짝수
+        dp[i] = dp[i-1]%mod + dp[i-2]%mod;
+      } else {
+        // 홀수
+        dp[i] = dp[i-1]*2%mod + dp[i-2]%mod;
+      }
+      dp[i] = dp[i]%mod;
+    }
+    return dp[n];
+  }
+}
