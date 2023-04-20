bdb9e2a05fcae92015e5d14dc0bef86e9b60c4e1
nttejun <nttejun@gmail.com>
2023-04-20 21:54:37 +0900

    [Add] 문자열 대소문자 바꾸기

--- src/programmers/Lv1StringPrint.java
+++ src/programmers/Lv1StringPrint.java
@@ -1,6 +1,7 @@
 package programmers;
 
 import java.util.Scanner;
+import org.junit.Test;
 
 public class Lv1StringPrint {
   public static void main(String[] args) {
@@ -11,12 +12,61 @@ public class Lv1StringPrint {
     System.out.println(a + b);
   }
 
-  public void getAB() {
+  public void A_와_B_출력하기() {
     Scanner sc = new Scanner(System.in);
     int a = sc.nextInt();
     int b = sc.nextInt();
 
     System.out.println("a = " + a);
     System.out.println("b = " + b);
+  }
+
+  public void 문자열_반복해서_출력하기(){
+    Scanner sc = new Scanner(System.in);
+    String str = sc.next();
+    int n = sc.nextInt();
+
+    for(int i=0; i<n; i++) {
+      System.out.print(str);
+    }
+  }
+
+  @Test
+  public void 대소문자_바꿔서_출력하기() {
+    Scanner sc = new Scanner(System.in);
+    String a = sc.next();
+
+    int big = 65;
+    int small = 97;
+
+    for(int i=0; i<a.length(); i++) {
+      int target = a.charAt(i);
+      if (target >= 65 && target <97) {
+        int gap = target - big;
+        gap += small;
+        System.out.print((char) gap);
+      } else {
+        int gap = target - small;
+        gap += big;
+        System.out.print((char) gap);
+      }
+    }
+  }
+
+  @Test
+  public void 대소문자_바꿔서_출력하기_다른방법() {
+    Scanner sc = new Scanner(System.in);
+    String a = sc.next();
+
+    String answer = "";
+    for (char c : a.toCharArray()) {
+      if (c >= 65 && c <97) {
+        answer += Character.toLowerCase(c);
+      } else {
+        answer += Character.toUpperCase(c);
+      }
+    }
+
+    System.out.println(answer);
   }
 }
