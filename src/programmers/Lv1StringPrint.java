1c7f8cfe0d66cee4af3c8e0c3d9a32da04a2bc46
nttejun <nttejun@gmail.com>
2023-04-20 21:02:47 +0900

    [Add] 문자열 다루기 a와 b

--- src/programmers/Lv1StringPrint.java
+++ src/programmers/Lv1StringPrint.java
@@ -10,4 +10,13 @@ public class Lv1StringPrint {
 
     System.out.println(a + b);
   }
+
+  public void getAB() {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    System.out.println("a = " + a);
+    System.out.println("b = " + b);
+  }
 }
