340fb66406bd1feece006f1c0bcf35e3cbd7a33d
nttejun <nttejun@gmail.com>
2023-04-23 17:56:20 +0900

    [Add] 해시문제 전화번호 목록 시간복잡도 nlogn 방법

--- src/programmers/HashLv2PhnNumberList.java
+++ src/programmers/HashLv2PhnNumberList.java
@@ -1,9 +1,11 @@
 package programmers;
 
+import java.util.Arrays;
 import org.junit.Test;
 
 public class HashLv2PhnNumberList {
 
+  // O(n^2)
   public boolean solution(String[] phone_book) {
     for (int i = 0; i < phone_book.length; i++) {
       for (int j = 0; j < phone_book.length; j++) {
@@ -11,6 +13,17 @@ public class HashLv2PhnNumberList {
         if(phone_book[i].startsWith(phone_book[j])) {
           return false;
         }
+      }
+    }
+    return true;
+  }
+
+  // O(nlogn)
+  public boolean solutionSortingVersion(String[] phone_book) {
+    Arrays.sort(phone_book);
+    for(int i = 0; i < phone_book.length-1; i++) {
+      if(phone_book[i+1].startsWith(phone_book[i])) {
+        return false;
       }
     }
     return true;
