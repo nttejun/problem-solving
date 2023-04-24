8aba830a7643af707d594be40b66a896276e0988
nttejun <nttejun@gmail.com>
2023-04-24 21:53:16 +0900

    [Add] 해시문제 의상

--- src/programmers/HashLv2Clothes.java
+++ src/programmers/HashLv2Clothes.java
@@ -1,12 +1,28 @@
 package programmers;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import org.junit.Assert;
 import org.junit.Test;
 
+
+// 각자 아무것도 없는 경우의 수를 가정해야 하기 때문에 1씩 더해서 곱해주고, 마지막에 아무것도 안입는 경우의 수 1을 빼줘야 한다.
+// https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=java
 public class HashLv2Clothes {
     public int solution(String[][] clothes) {
-        int answer = 0;
-        return answer;
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < clothes.length; i++) {
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0)+1);
+        }
+
+        int answer = 1;
+        Iterator<Integer> iter = map.values().iterator();
+        while(iter.hasNext()) {
+            answer *= iter.next().intValue() + 1;
+        }
+
+        return answer-1;
     }
 
     @Test
@@ -16,9 +32,9 @@ public class HashLv2Clothes {
         clothes[0][1] = "face";
         clothes[1][0] = "blue_sunglasses";
         clothes[1][1] = "face";
-        clothes[2][1] = "smoky_makeup";
-        clothes[2][2] = "face";
-        Assert.assertEquals(solution(clothes), 3);
+        clothes[2][0] = "smoky_makeup";
+        clothes[2][1] = "face";
+        Assert.assertEquals(3, solution(clothes));
     }
 
     @Test
@@ -30,6 +46,6 @@ public class HashLv2Clothes {
         clothes[1][1] = "eyewear";
         clothes[2][0] = "green_turban";
         clothes[2][1] = "headgear";
-        Assert.assertEquals(solution(clothes), 5);
+        Assert.assertEquals(5, solution(clothes));
     }
 }
