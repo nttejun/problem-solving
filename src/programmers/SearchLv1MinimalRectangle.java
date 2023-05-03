8c8b629337fe9424dd7d30c0d191a4a440425c0b
nttejun <nttejun@gmail.com>
2023-05-03 20:51:36 +0900

    [Add] 완전탐색 최소 직사각형

--- src/programmers/SearchLv1MinimalRectangle.java
+++ src/programmers/SearchLv1MinimalRectangle.java
@@ -3,55 +3,26 @@ package programmers;
 import org.junit.Assert;
 import org.junit.Test;
 
+
+// Max 값을 찾고, 작은 값들 중에서 최대값을 찾는다
 public class SearchLv1MinimalRectangle {
 
   public int solution(int[][] sizes) {
-    int maxA = 0;
-    int maxB = 0;
-
-
-    int[][] temp = new int[1][2];
-    boolean isFirstBig = false;
+    int max = 0;
+    int minMax = 0;
 
     for (int i = 0; i < sizes.length; i++) {
-      if (maxA < sizes[i][0]) {
-        maxA = i;
-      }
-      if (maxB < sizes[i][1]) {
-        maxB = i;
+      for (int j = 0; j < sizes[i].length; j++) {
+        if (max < sizes[i][j]) {
+          max = sizes[i][j];
+        }
       }
     }
 
-    if (sizes[maxA][0] >= sizes[maxB][0]) {
-      isFirstBig = true;
-      temp[0][0] = sizes[maxA][0];
-      temp[0][1] = sizes[maxA][1];
-    } else {
-      isFirstBig = false;
-      temp[0][0] = sizes[maxB][0];
-      temp[0][1] = sizes[maxB][1];
-    }
-
-    int minTempBigIndex = -1;
     for (int i = 0; i < sizes.length; i++) {
-      if (temp[0][1] < sizes[i][1]) {
-        minTempBigIndex = i;
-      }
+      minMax = Math.max(minMax, Math.min(sizes[i][0], sizes[i][1]));
     }
-
-    if (minTempBigIndex == -1) {
-      if (isFirstBig) {
-        return sizes[maxA][0] * sizes[maxA][1];
-      } else {
-        return sizes[maxB][0] * sizes[maxB][1];
-      }
-    } else {
-      if (isFirstBig) {
-        return sizes[maxA][0] * sizes[minTempBigIndex][1];
-      } else {
-        return sizes[maxB][0] * sizes[minTempBigIndex][1];
-      }
-    }
+    return max*minMax;
   }
 
   @Test
