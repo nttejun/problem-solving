c3ee5ba54bbb7412ff3a996258466f75d4d75bfa
nttejun <nttejun@gmail.com>
2023-05-21 17:21:46 +0900

    [Add] 탐색 모음사전 점화식으로 해결

--- src/programmers/SearchLv2Dictionary.java
+++ src/programmers/SearchLv2Dictionary.java
@@ -6,8 +6,16 @@ import org.junit.Test;
 public class SearchLv2Dictionary {
 
   public int solution(String word) {
-    int answer = 0;
-    return answer;
+    String str = "AEIOU";
+    int[] x = {781, 156, 31, 6, 1};
+    int index,result = word.length();
+
+    for(int i=0; i<word.length(); i++) {
+      index = str.indexOf(word.charAt(i));
+      result+=x[i]*index;
+    }
+
+    return result;
   }
 
   @Test
