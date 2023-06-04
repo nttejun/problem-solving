5f46ede4855fe0330c2006885d4c9a493fdc1a7d
nttejun <nttejun@gmail.com>
2023-06-04 22:48:22 +0900

    [Add] dfs bfs 아이템 줍기

--- src/programmers/DfsBfsLv3ItemGet.java
+++ src/programmers/DfsBfsLv3ItemGet.java
@@ -1,5 +1,7 @@
 package programmers;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,14 +11,47 @@ public class DfsBfsLv3ItemGet {
   static int answer;
 
   public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
-    int answer = 0;
+    answer = 0;
     map = new int[101][101];
 
     for (int i = 0; i < rectangle.length; i++) {
       fill(2*rectangle[i][0], 2*rectangle[i][1], 2*rectangle[i][2], 2*rectangle[i][3]);
     }
 
-    return answer;
+    bfs(2*characterX, 2*characterY, 2*itemX, 2*itemY);
+
+    return answer/2;
+  }
+
+  static int[] dx = {-1, 0, 0, 1};
+  static int[] dy = {0, -1, 1, 0};
+  private void bfs(int startx, int starty, int itemx, int itemy) {
+    boolean[][] visited = new boolean[101][101];
+    Queue<Integer> queue = new LinkedList<>();
+    queue.add(startx);
+    queue.add(starty);
+
+    while(!queue.isEmpty()) {
+      int x = queue.poll();
+      int y = queue.poll();
+
+      for (int i = 0; i < 4; i++) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+        if(!check(nx, ny)) continue;
+        if(map[nx][ny] != 1 || visited[nx][ny]) continue;
+
+        map[nx][ny] = map[x][y]+1;
+        if(nx==itemx && ny == itemy) {
+          answer = (answer == 0) ? map[nx][ny]:Math.min(answer,map[nx][ny]);
+          continue;
+        }
+
+        visited[nx][ny] = true;
+        queue.add(nx);
+        queue.add(ny);
+      }
+    }
   }
 
   private void fill(int x1, int y1, int x2, int y2) {
@@ -30,6 +65,11 @@ public class DfsBfsLv3ItemGet {
         }
       }
     }
+  }
+
+  public boolean check(int x, int y){
+    if(x<0||y<0||x>100||y>100) return false;
+    return true;
   }
 
 
