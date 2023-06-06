45322c525bb2bf23de52bcc06ecd2d820df4bb40
nttejun <nttejun@gmail.com>
2023-06-06 11:31:41 +0900

    [Add] heap 디스크 컨트롤러

---
+++ src/programmers/HeapLv3DiskController.java
@@ -0,0 +1,19 @@
+package programmers;
+
+import java.util.PriorityQueue;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HeapLv3DiskController {
+  public int solution(int[][] jobs) {
+    int answer = 0;
+    PriorityQueue pQueue = new PriorityQueue();
+    return answer;
+  }
+
+  @Test
+  public void solution() {
+    int[][] jobs = new int[][]{{0,3}, {1,9}, {2,6}};
+    Assert.assertEquals(9, jobs);
+  }
+}
