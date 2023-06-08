a7b81534b2ab8b773f917962682824b69d14fc50
nttejun <nttejun@gmail.com>
2023-06-09 07:17:08 +0900

    [Add] heap 디스크 컨트롤러

--- src/programmers/HeapLv3DiskController.java
+++ src/programmers/HeapLv3DiskController.java
@@ -1,5 +1,7 @@
 package programmers;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 import org.junit.Assert;
 import org.junit.Test;
@@ -7,13 +9,37 @@ import org.junit.Test;
 public class HeapLv3DiskController {
   public int solution(int[][] jobs) {
     int answer = 0;
-    PriorityQueue pQueue = new PriorityQueue();
-    return answer;
+
+    Arrays.sort(jobs, Comparator.comparingInt(job -> job[1]));
+
+    PriorityQueue<int[]> queue = new PriorityQueue<>(Comparator.comparingInt(job -> job[1]));
+
+    int index = 0;
+    int count = 0;
+    int total = 0;
+    int end = 0;
+    while (count < jobs.length) {
+
+      while (index < jobs.length && jobs[index][0] <= end) {
+        queue.add(jobs[index++]);
+      }
+
+      if(queue.isEmpty()) {
+        end = jobs[index][0];
+      } else {
+        int[] cur = queue.poll();
+        total += cur[1] + end - cur[0];
+        end += cur[1];
+        count++;
+      }
+    }
+
+    return total / jobs.length;
   }
 
   @Test
   public void solution() {
     int[][] jobs = new int[][]{{0,3}, {1,9}, {2,6}};
-    Assert.assertEquals(9, jobs);
+    Assert.assertEquals(9, solution(jobs));
   }
 }
