0ba2bc3a18bc6cf28c41c0a86ae7cdb711a3cf64
nttejun <nttejun@gmail.com>
2023-07-12 07:18:00 +0900

    [Modify] 그래프 방의 개수

--- src/programmers/GraphLv3RoomCount.java
+++ src/programmers/GraphLv3RoomCount.java
@@ -3,6 +3,7 @@ package programmers;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Objects;
+import org.junit.Test;
 
 public class GraphLv3RoomCount {
 
@@ -79,4 +80,9 @@ public class GraphLv3RoomCount {
     return edge;
   }
 
+  @Test
+  public void solution() {
+    int[] arrows = {6, 6, 6, 4, 4, 4, 2, 2, 2, 0, 0, 0, 1, 6, 5, 5, 3, 6, 0};
+    solution(arrows);
+  }
 }
