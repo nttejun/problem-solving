dcc60db7de1baf9180f2f1e6a7c605a7ab01391d
nttejun <nttejun@gmail.com>
2023-07-26 06:53:32 +0900

    [Modify] 힙 Lv2 더 맵게 리펙토링 추가

--- src/programmers/HeapLv2MoreHot.java
+++ src/programmers/HeapLv2MoreHot.java
@@ -49,6 +49,28 @@ public class HeapLv2MoreHot {
     return answer;
   }
 
+  public int solutionRefactoring(int[] scoville, int K) {
+    PriorityQueue<Integer> q = new PriorityQueue<>();
+
+    for(int i = 0; i < scoville.length; i++)
+      q.add(scoville[i]);
+
+    int count = 0;
+    while(q.size() > 1 && q.peek() < K){
+      int weakHot = q.poll();
+      int secondWeakHot = q.poll();
+
+      int mixHot = weakHot + (secondWeakHot * 2);
+      q.add(mixHot);
+      count++;
+    }
+
+    if(q.size() <= 1 && q.peek() < K)
+      count = -1;
+
+    return count;
+  }
+
   @Test
   public void SOLUTION_TEST_1() {
     int k = 7;
