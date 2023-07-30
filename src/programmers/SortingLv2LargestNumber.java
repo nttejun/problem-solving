ebe02e8103093ae9883e8e8eb7d6cedf1764ab68
nttejun <nttejun@gmail.com>
2023-07-30 12:08:21 +0900

    [Modify] 정렬 lv2 가장 큰 수 compare to

--- src/programmers/SortingLv2LargestNumber.java
+++ src/programmers/SortingLv2LargestNumber.java
@@ -24,8 +24,9 @@ public class SortingLv2LargestNumber {
         return (n2+n1).compareTo(n1+n2);
       }
     });
+
     for(int i = 0; i < num.length; i++)
-      answer += num[i];
+        answer += num[i];
     if(except) answer = "0";
     return answer;
   }
@@ -34,5 +35,38 @@ public class SortingLv2LargestNumber {
   public void TEST_성공케이스() {
     int[] numbers = new int[]{6, 10, 2};
     Assert.assertEquals("6210", solution(numbers));
+  }
+
+  @Test
+  public void TEST_성공케이스_2() {
+    int[] numbers = new int[]{3, 30, 34, 5, 9};
+    Assert.assertEquals("9534330", solution(numbers));
+  }
+
+  @Test
+  public void TEST_COMPARE_TO() {
+    String[] nums = new String[]{"1", "10", "2", "29", "20", "31", "34"};
+
+    Arrays.sort(nums, new Comparator<String>() {
+      @Override
+      public int compare(String o1, String o2) {
+        return (o2+o1).compareTo(o1+o2);
+      }
+    });
+
+    String resultAsc = "";
+    for (String n : nums) resultAsc += n + " ";
+    Assert.assertEquals("34 31 29 2 20 1 10 ", resultAsc);
+
+    Arrays.sort(nums, new Comparator<String>() {
+      @Override
+      public int compare(String o1, String o2) {
+        return (o1+o2).compareTo(o2+o1);
+      }
+    });
+
+    String resultDesc = "";
+    for (String n : nums) resultDesc += n + " ";
+    Assert.assertEquals("10 1 20 2 29 31 34 ", resultDesc);
   }
 }
