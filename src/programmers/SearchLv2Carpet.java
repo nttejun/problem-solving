502f0ed0dfcbbe7f6474eba69bcbde7d07342248
nttejun <nttejun@gmail.com>
2023-08-01 08:42:04 +0900

    [Modify] 완전탐색 lv2 카펫 다른 풀이

--- src/programmers/SearchLv2Carpet.java
+++ src/programmers/SearchLv2Carpet.java
@@ -45,6 +45,27 @@ public class SearchLv2Carpet {
     }
   }
 
+  public int[] solutionNormal(int brown, int yellow) {
+    int[] answer = new int[2];
+    int area = brown + yellow;
+
+    for (int i = 1; i <= area; i++) {
+      int row = i;
+      int col = area / row;
+
+      if (row > col)
+        continue;
+
+      if ((row - 2) * (col - 2) == yellow) {
+        answer[0] = col;
+        answer[1] = row;
+        return answer;
+      }
+    }
+
+    return answer;
+  }
+
   @Test
   public void solution() {
     int[] answer = solution(24, 24);
