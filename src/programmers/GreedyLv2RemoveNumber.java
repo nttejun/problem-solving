c39672b0c8336baf59d765689121c20e6a5834d4
nttejun <nttejun@gmail.com>
2023-08-06 20:33:52 +0900

    [Modify] 그리디 lv2 큰 수 만들기 설명추가

--- src/programmers/GreedyLv2RemoveNumber.java
+++ src/programmers/GreedyLv2RemoveNumber.java
@@ -12,6 +12,7 @@ public class GreedyLv2RemoveNumber {
 
     for (int i = 0; i < number.length(); i++) {
       char c = number.charAt(i);
+      // next 숫자 c 보다 작은 값이 stack 에 존재한다면 k가 0이 되기 전까지 작은 숫자는 스택에서 제거
       while (!stack.isEmpty() && stack.peek() < c && k-- > 0) {
         stack.pop();
       }
